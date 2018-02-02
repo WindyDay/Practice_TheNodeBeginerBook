7a37508cb2af22515bad3071f3027d73180559c4
Nguyễn Đình Sơn <001.IceTea@gmail.com>
2018-02-03 03:28:19 +0700

    Lean to modularize functions

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 var http = require("http");
 
-const PORT = process.env.PORT || 9000;
+function start(){
+    const PORT = process.env.PORT || 9000;
 
-http.createServer(function(request, response){
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello word");
-    response.end();
-}).listen(PORT, () => {
-    console.log("Server is running on port " + PORT + "!")
-});
+    function onRequest(request, response){
+        console.log("Request received.")
+        response.writeHead(200, {"Content-Type": "text/plain"});
+        response.write("Hello word");
+        response.end();
+    }
+    
+    http.createServer(onRequest).listen(PORT, () => {
+        console.log("Server is running on port " + PORT + "!")
+    });
+}
+
+exports.start = start;
